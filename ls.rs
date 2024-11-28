98815d19b83f9aaf5e4edd873ac089d832ec680f
samicpp <samichentouf+git2@gmail.com>
2024-11-28 18:36:33 +0000

    test

--- ls.rs
+++ ls.rs
@@ -2,5 +2,13 @@ use std::env;
 use std::fs;
 
 fn main(){
-  ;
+  let mut args: Vec<String>=env::args().collect();
+    let cmd=args[0].clone();
+    args.remove(0);
+
+    if args.len()!=1{
+        println!("{cmd}: Need 1 argument");
+        std::process::exit(1);
+    };
+    
 }
